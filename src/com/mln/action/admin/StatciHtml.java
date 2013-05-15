b7f9eead13be306581ad11fd8338a0bf39950df9
geeksun <geeksun@163.com>
2013-05-15 18:09:40 +0800

    配置文件增加静态属性

--- src/com/mln/action/admin/StatciHtml.java
+++ src/com/mln/action/admin/StatciHtml.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mln.bean.ProductCategory;
 import com.mln.db.DBHandler;
+import com.mln.util.PropertiesUtil;
+import com.mln.util.StringUtil;
 
 /**
  * Description Generate Static Html
@@ -34,61 +38,71 @@ public class StatciHtml {
 	 */
 	@SuppressWarnings("unchecked")
 	@RequestMapping(value="productCategory")
-	public String productCategory(){
+	public String productCategory(HttpServletRequest request, Model model){
 		String _sql = "select * from product_category";
 		List<ProductCategory> _list = (List<ProductCategory>) DBHandler.queryBeanList(_sql, ProductCategory.class);
 		StringBuilder _bd = new StringBuilder();
 		
-		Map<ProductCategory, List<ProductCategory>> m = new HashMap<ProductCategory, List<ProductCategory>>();
+		
+		List<ProductCategory> _list_1 = new ArrayList<ProductCategory>();
 		for(ProductCategory p:_list){
-			ProductCategory _p = null;
+			//  one level menu
 			if(p.getPid().equals("0")){
-				_p = p;
-				String _id = _p.getId();
-				List<ProductCategory> _l = new ArrayList<ProductCategory>();
-				for(ProductCategory p2:_list){
-					if(p2.getPid().equals(_id)){
-						_l.add(p2);
-					}
-				}
-				m.put(_p, _l);
+				_list_1.add(p);
 			}
 		}
 		
+		Map<ProductCategory, List<ProductCategory>> _map_2 = new HashMap<ProductCategory, List<ProductCategory>>();
+		List<ProductCategory> _list_2 = null;
+		for(ProductCategory p:_list_1){
+			_list_2 = new ArrayList<ProductCategory>();
+			String id = p.getId();
+			for(ProductCategory p2:_list){
+				if(p2.getPid().equals(id)){
+					_list_2.add(p2);
+				}
+			}
+			_map_2.put(p, _list_2);
+		}
 		
-		for(ProductCategory p:_list){
-			String id = null ;
-			if(p.getPid().equals("0")){
-				id = p.getId();
+		for(ProductCategory p:_list_1){
+			_bd.append("<li>");
+			_bd.append("<a href='/product?fid=");
+			_bd.append(p.getId());
+			_bd.append("'>");
+			_bd.append(p.getName());
+			_bd.append("</a>");
+			_bd.append("<ul>");
+			_list_2 = _map_2.get(p);
+			for(ProductCategory _p2:_list_2){
 				_bd.append("<li>");
 				_bd.append("<a href='/product?fid=");
-				_bd.append(id);
+				_bd.append(_p2.getId());
 				_bd.append("'>");
-				_bd.append(p.getName());
-				_bd.append("</a>");
-				_bd.append("<ul>");
-			} else if(p.getPid().equals(id)){
-				_bd.append("<li>");
-				_bd.append("<a href='/product?fid=");
-				_bd.append(p.getId());
-				_bd.append("'>");
-				_bd.append(p.getName());
+				_bd.append(_p2.getName());
 				_bd.append("</a>");
 				_bd.append("</li>");
 			}
-			
+			_bd.append("</ul>");
+			_bd.append("</li>");
 		}
 		
+		//System.out.println(_bd.toString());
+		@SuppressWarnings("deprecation")
+		String filePath = request.getRealPath("")+"/html/product/category.htm";
+		StringUtil.generateHtml(filePath, _bd.toString());
+		model.addAttribute("id", PropertiesUtil.getValue("productCategory"));
 		
-		
-		
-		
-		
-		return "admin/static_html";
+		return "admin/pub/_operate_status";
 	}
 	
-	
-	
-	
+	/**
+	 * 新闻菜单
+	 */
+	@RequestMapping(value="newsMenu")
+	public String newsMenu(HttpServletRequest request, Model model){
+		
+		return "";
+	}
 
 }
