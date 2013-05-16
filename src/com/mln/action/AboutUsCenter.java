26a59a11e3a8de62d76ed5a030128eeccc796336
geeksun <geeksun@163.com>
2013-05-16 14:38:49 +0800

    联系我们和关于我们类名更换新名

---
+++ src/com/mln/action/AboutUsCenter.java
@@ -0,0 +1,39 @@
+package com.mln.action;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mln.bean.About;
+import com.mln.db.DBHandler;
+
+/**
+ * Description 公司简介
+ * 2013-4-24
+ * @author usr1999
+ */
+@Controller
+@RequestMapping(value="about")
+public class AboutUsCenter {
+	
+	@RequestMapping(value="")
+	public String exec(){
+		return "about-us";
+	}
+	
+	@RequestMapping(value="{id}")
+	public String structure(@PathVariable Integer id, Model model, HttpServletRequest request){
+		String _sql = "select * from about where id=?";
+		About _info = (About) DBHandler.queryBean(_sql, About.class, id);
+		
+		model.addAttribute("_info", _info);
+		
+		return "about/_info";
+	}
+	
+	
+	
+}
